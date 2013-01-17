6549b9e317d7efc3a0c2884742c2f9d44f2a06c4
dcook1021@gmail.com <dcook1021@gmail.com@5ac20e6e-5faa-39b9-9036-974e8038399e>
2013-01-17 22:13:19 +0000

    Added default classes in ErrorHandler.java

--- src/ErrorHandler.java
+++ src/ErrorHandler.java
@@ -1,5 +1,38 @@
 package antelope;
 
 public interface ErrorHandler {
-    public void handle(String message);
+    public void handle(String file, int line, String message);
+    
+    public static class Message implements Comparable<Message> {
+        private static int ID = 0;
+        public final String file, message;
+        public final int line, id;
+        public Message(String file, int line, String message) {
+            this.file = file; this.line = line; this.message = message; id = ID++;
+        }
+        public int compareTo(Message other) {
+            int value = file.compareTo(other.file);
+            return (value != 0)? value :
+                (line != other.line)? (line < other.line ? -1 : 1) :
+                (id < other.id ? -1 : 1);
+        }
+    }
+
+    public static class Printer implements ErrorHandler {
+        protected java.io.PrintStream printStream;
+        public Printer(java.io.PrintStream printStream) {
+            this.printStream = printStream;
+        }
+        public void handle(String file, int line, String message) {
+            printStream.println(file+": "+line+": "+message);
+        }
+    }
+
+    public static class Sorter implements ErrorHandler {
+        public final java.util.TreeSet<Message> messages;
+        public Sorter() { messages = new java.util.TreeSet<Message>(); }
+        public void handle(String file, int line, String message) {
+            messages.add(new Message(file, line, message));
+        }
+    }
 }
