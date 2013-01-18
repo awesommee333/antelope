91d130163c4c165dc4c16321cdf49ab5f5424779
dcook1021@gmail.com <dcook1021@gmail.com@5ac20e6e-5faa-39b9-9036-974e8038399e>
2013-01-18 03:13:52 +0000

    Added "clearLine" method to TokenSource. Revised Preprocessor a lot (need to modify it to loop rather than recurse, returning only when it hits a non-directive).

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -3,16 +3,16 @@ import java.util.LinkedList;
 
 public final class Preprocessor implements TokenSource {
     private final java.util.TreeSet<Token> defined;
-    private final LinkedList<Token> allocations;
-    private final LinkedList<Token> assemblies;
+    private final LinkedList<Token> allocations; // Will eventually replace with proper constructs
+    private final LinkedList<Token> assemblies;  // Ditto.
     private final TokenSource source;
-    private int lastLine;
+    private boolean notFirst;
     
     public Preprocessor(TokenSource source) {
-        this.source = source;
-        lastLine = getLine() - 1;
-        assemblies = new LinkeList<Token>();
         allocations = new LinkeList<Token>();
+        assemblies = new LinkeList<Token>();
+        this.source = source;
+        notFirst = false;
     }
     
     public Preprocessor(String file, ErrorHandler handler) {
@@ -22,138 +22,48 @@ public final class Preprocessor implements TokenSource {
     public int getLine() { return source.getLine(); }
     public String getName() { return source.getName(); }
     
-    private void error(String message) {
-        Token t;
-        int line = getLine();
-        do { Token t = source.nextToken(); }
-        while(line == getLine() && !t.isEOF());
-        handler.handle(getName(), line, message);
+    private Token retry(String errorMessage, int line) {
+        handler.handle(getName(), line, errorMessage);
+        source.clearLine();
+        return nextToken();
     }
 
     public Token nextToken() {
-        Token t = source.nextToken();
-        if(t != Token.POUND) return t;
-        t = source.nextToken();
         int line = getLine();
+        Token t = source.nextToken();
+        if(t != Token.POUND) {
+            notFirst = true;
+            return t;
+        }
+        if(notFirst) {
+            int nextLine = getLine();
+            if(line == nextLine)
+                handler.handle(getName(), line, "Directive not on its own line");
+            line = nextLine;
+            notFirst = true;
+        }
+        
+        t = source.nextToken();
+        
+        if(line != getLine())
+            return retry("Missing directive after '#'", line);
         
         if(t == Token.DEFINE) {
+            String msg = "Missing identifier after #define";
             do {
                 t = source.nextToken();
+                if(line != getLine()) {
+                    handler.handle(getName(), line, msg);
+                    return t;
+                }
                 if(t.isIdentifier()) { defined.add(t); }
-                else { error("Identifier expected after #define. Found: "+t); }
-            } while(line == getLine());
+                else { return retry("Identifier expected after #define. Found: "+t); }
+                msg = "Missing identifier after comma";
+                t = source.nextToken();
+            } while(t == Token.COMMA && line == getLine());
+            return nextToken();
         }
         
         
-        Token t;
-        for(t = nextToken(); t != Token.EOS && t != Token.DIR_END; t = nextToken()) {
-            int l = line;
-            if(t == Token.ENVIRONMENT) {
-                if(nextLineToken() != Token.ASSIGN) { error("Missing '=' after 'environment'"); }
-                else {
-                    Token e = nextToken();
-                    if(e != null && (e.isIdent() || e.isKeyword())) {
-                        if(nextToken() != Token.SEMI) { error("Missing semicolon after environment", l); }
-                        if(environment != null && environment != e) {
-                            error("Duplicate environment selection (already set to '"+environment+"')");
-                        } else { environment = e; }
-                    } else { error("Identifier expected after 'environment ='. Found: "+e); }
-                }
-            }
-            else if(t.isDirective()) {
-                Token next = nextLineToken();
-                if(next == null) {
-                    error("Missing arguments after "+t);
-                    if(t != Token.DIR_ENVIRONMENT && t != Token.DIR_FOR) {
-                        continue;
-                    }
-                }
-
-                boolean lineErr = true;
-
-                if(t == Token.DIR_INCLUDE) {
-                    String incFile = next.format(false);
-                    if(incFile == null) { error("Invalid file name: "+next); }
-                    else if(legalEnvironment) {
-                        StringBuilder src = getSource(incFile);
-                        if(src == null) { error("Unable to process file: "+incFile); }
-                        else {
-                            Preprocessor p = new Preprocessor(src, incFile, environment, environmentUsed);
-                            if(p.tokens.size()+p.allocations.size()+p.assemblies.size() > 0) {
-                                allocations.consume(p.allocations);
-                                assemblies.consume(p.assemblies);
-                                errors.consume(p.errors);
-                                tokens.consume(p.tokens);
-                                prevLine = 0; // triggers a fileMarker
-                            }
-                        }
-                    }
-                }
-                else if(t == Token.DIR_FOR || t == Token.DIR_ENVIRONMENT) {
-                    boolean isEnv = (t == Token.DIR_ENVIRONMENT);
-                    boolean pass = false;
-                    do {
-                        pass = (pass || next == environment);
-                        if(!next.isIdent() && !next.isKeyword()) {
-                            error("Identifier expected after "+t+". Found: "+next);
-                        } else if(t == Token.DIR_ENVIRONMENT) { break; }
-                        next = nextLineToken();
-                    } while(next != null);
-
-                    boolean wasLegal = legalEnvironment;
-                    legalEnvironment = (pass && legalEnvironment && !inEnv);
-                    if(isEnv && pass) {
-                        if(environmentUsed)
-                            error("Duplicate environment definition for "+environment, l);
-                        environmentUsed = true;
-                    }
-                    if(tokenize(isEnv || inEnv) != Token.DIR_END) {
-                        error("Missing #end to match "+t+" on line "+l);
-                    } else { t = Token.DIR_END; }
-                    legalEnvironment = wasLegal;
-                }
-                else { // By default, t is #allocate or #assembly
-                    boolean isAlloc = (t == Token.DIR_ALLOCATE);
-                    if(!inEnv) {
-                        error("Illegal use of "+t+" without #environment");
-                        lineErr = false;
-                    } else if(!next.isString()) {
-                        error("String value expected after "+t+". Found: "+next);
-                        lineErr = false;
-                    } else {
-                        Token n = ((isAlloc)? nextLineToken() : null);
-                        boolean minus = (n == Token.MINUS);
-                        if(minus) { n = nextLineToken(); }
-
-                        if(isAlloc && (n == null || !n.isNumber())) {
-                            error("Number expected after #allocate "+next);
-                            lineErr = false;
-                        } else if(legalEnvironment) {
-                            allocations.add(t);
-                            allocations.add(next);
-                            if(minus) { allocations.add(Token.makeNumber(-n.number)); }
-                            else if(isAlloc) { allocations.add(n); }
-                        }
-                    }
-                }
-
-                // Trim the rest of the line:
-
-                Token n = nextLineToken();
-                if(n != null) {
-                    StringBuilder err = new StringBuilder("Excess code found after "+t+":");
-                    while(n != null) { err.append(" "+n); n = nextLineToken(); }
-                    if(lineErr) { error(err.toString(), l); }
-                }
-            }
-            else if(legalEnvironment) {
-                if(line != prevLine) {
-                    prevLine = line;
-                    tokens.add(Token.makeFileMarker(file,line));
-                }
-                tokens.add(t);
-            }
-        }
-        return t;
     }
 }
--- src/TokenSource.java
+++ src/TokenSource.java
@@ -1,12 +1,14 @@
 package antelope;
 
 public interface TokenSource {
+    public void clearLine();
     public Token nextToken();
     public String getName();
     public int getLine();
     
-    public static class Default implements TokenSource {
-        protected int line;
+    public static final class Default implements TokenSource {
+        private int line;
+        private Token next;
         public final String name;
         public final StringBuilder source;
         public final ErrorHandler handler;
@@ -15,16 +17,21 @@ public interface TokenSource {
         public Default(StringBuilder source, String name, ErrorHandler handler) {
             this.source = source; this.name = name;
             this.handler = handler; line = 1;
+            next = Token.nextToken();
         }
         public Token nextToken() {
-            Token t;
             do {
-                t = Token.nextToken(source);
-                if(t.isError()) { handler.handle(name, line, t.value); }
-                else if(t.isNewLine()) { line++; }
-                else if(!t.isComment()) { break; }
+                if(next.isError()) { handler.handle(name, line, next.value); }
+                else if(next.isNewLine()) { line++; }
+                else if(!next.isComment()) { break; }
+                next = Token.nextToken(source);
             } while(true);
-            return t;
+            return next;
+        }
+        public void clearLine() {
+            while(!next.isEOF() && !next.isNewLine()) {
+                next = Token.nextToken(source);
+            }
         }
         public static Default Get(String file, ErrorHandler handler) {
             javax.swing.JTextArea jta = new javax.swing.JTextArea();
