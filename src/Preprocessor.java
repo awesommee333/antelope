37b987d047a59682bff3581e649545c3814d4949
dcook1021@gmail.com <dcook1021@gmail.com@5ac20e6e-5faa-39b9-9036-974e8038399e>
2013-01-19 20:18:07 +0000

    Significant progress on Preprocessor.java

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -9,12 +9,12 @@ public final class Preprocessor implements TokenSource {
     public final TokenSource source;
     private boolean first;
 
+    private Preprocessor(TokenSource src, ErrorHandler hndlr, LinkedList<Token> asm, LinkedList<Token> alloc) {
+        source = src; handler = hndlr; allocations = alloc; assemblies = asm; first = true;
+    }
+
     public Preprocessor(TokenSource source, ErrorHandler handler) {
-        allocations = new LinkedList<Token>();
-        assemblies = new LinkedList<Token>();
-        this.handler = handler;
-        this.source = source;
-        first = true;
+        this(source, handler, new LinkedList<Token>(), new LinkedList<Token>());
     }
 
     public Preprocessor(String file, ErrorHandler handler) {
@@ -33,37 +33,86 @@ public final class Preprocessor implements TokenSource {
     }
 
     public Token nextToken() {
-        int line = getLine();
+        int prevLine = getLine();
         Token t = source.nextToken();
         while(t == Token.POUND) {
-            int before = line;
-            int after = getLine();
+            int line = getLine();
             t = source.nextToken();
-            line = getLine();
-            if(before == after && !first) {
-                handler.handle(getName(), before, "Directive not on its own line");
+            if(line == prevLine && first) {
+                handler.handle(getName(), prevLine, "Directive not on its own line");
+                first = false;
             }
             else if(line != getLine()) {
                 t = clearLine("Missing directive after '#'", line);
             }
             else if(t == Token.DEFINE) {
-                String msg = "Missing identifier after #define";
+                String msg = "Missing arguments after #define";
                 do {
                     t = source.nextToken();
-                    if(line != getLine()) {
+                    if(line != getLine())
                         handler.handle(getName(), line, msg);
-                        break;
-                    }
-                    if(t.isIdentifier()) { defined.add(t); }
-                    else {
+                    else if(t.isIdentifier())
+                        defined.add(t);
+                    else
                         t = clearLine("Identifier expected after #define. Found: "+t, line);
-                        break;
-                    }
                     msg = "Missing identifier after comma";
-                    t = source.nextToken();
                 } while(t == Token.COMMA && line == getLine());
             }
-            // else if(...rest of directives...) {  }
+            else if(t == Token.ASSEMBLY) {
+                t = nextToken();
+                if(line != getLine())
+                    handler.handle("Missing arguments after #assembly");
+                else if(!t.isString())
+                    t = clearLine("Code for #assembly must be a string literal");
+                else
+                    allocations.add(t);
+            }
+            else if(t == Token.ALLOCATE) {
+                t = nextToken();
+                if(line != getLine())
+                    handler.handle("Missing arguments after #allocate");
+                else if(!t.isString())
+                    t = clearLine("Address for #allocate must be a string literal");
+                else {
+                    int limit = 0;
+                    Token addr = t;
+                    t = source.nextToken();
+
+                    if(line == getLine()) {
+                        boolean minus = (t = Token.MINUS);
+                        if(!minus && t != Token.PLUS)
+                            t = clearLine("Unexpected symbol after #allocate: "+t, line);
+                        else {
+                            t = source.nextToken();
+                            if(line == getLine() && !t.isInteger())
+                                t = clearLine("Unexpected symbol after #allocate: "+t, line);
+                            limit = (minus ? -t.number : t.number);
+                        }
+                    }
+
+                    allocations.add(addr);
+                    allocations.add(Token.makeNumber(limit));
+                }
+            }
+            else if(t == Token.INCLUDE) {
+                // INSERT CODE!
+            }
+            else if(t == Token.IF) {
+                // INSERT CODE!
+            }
+            else if(t == Token.ERROR) {
+                t = nextToken();
+                if(line !- getLine())
+                    handler.handle("<INSERT ERROR MESSAGE>");
+                else if(t.isString())
+                    handler.handle(t.toString());
+                else
+                    t = clearLine("Error message must be a string literal", line);
+            }
+            else {
+                t = clearLine("Invalid directive: #"+t);
+            }
+            prevLine = line;
         }
         first = false;
         return t;
