010a81d67a4ed4f58a8ba4992cf138d0870a37a1
dcook1021@gmail.com <dcook1021@gmail.com@5ac20e6e-5faa-39b9-9036-974e8038399e>
2013-01-19 06:33:44 +0000

    Removed the clearLine method from TokenSource.Default. Intermittent work on Preprocessor.java

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -6,64 +6,67 @@ public final class Preprocessor implements TokenSource {
     private final LinkedList<Token> allocations; // Will eventually replace with proper constructs
     private final LinkedList<Token> assemblies;  // Ditto.
     private final TokenSource source;
-    private boolean notFirst;
-    
+    private boolean first;
+
     public Preprocessor(TokenSource source) {
         allocations = new LinkeList<Token>();
         assemblies = new LinkeList<Token>();
         this.source = source;
-        notFirst = false;
+        first = true;
     }
-    
+
     public Preprocessor(String file, ErrorHandler handler) {
         super(new TokenSource.Default.Get(file, handler));
     }
-    
+
     public int getLine() { return source.getLine(); }
     public String getName() { return source.getName(); }
-    
-    private Token retry(String errorMessage, int line) {
-        handler.handle(getName(), line, errorMessage);
-        source.clearLine();
-        return nextToken();
+
+    private Token clearLine(String errorMessage, int line) {
+        source.handler.handle(getName(), line, errorMessage);
+        line = getLine(); Token t;
+        do { t = source.nextToken(); }
+        while(line == getLine() && !t.isNewLine());
+        return t;
     }
 
     public Token nextToken() {
-        int line = getLine();
-        Token t = source.nextToken();
-        if(t != Token.POUND) {
-            notFirst = true;
-            return t;
+        while(true) {
+            int line = getLine();
+            Token t = source.nextToken();
+            if(t != Token.POUND) break;
+
+            if(!first) {
+                int nextLine = getLine();
+                if(line == getLine())
+                    handler.handle(getName(), line, "Directive not on its own line");
+                continue;
+            }
+
+            t = source.nextToken();
+
+            if(line != getLine())
+                return retry("Missing directive after '#'", line);
+
+            if(t == Token.DEFINE) {
+                String msg = "Missing identifier after #define";
+                do {
+                    t = source.nextToken();
+                    if(line != getLine()) {
+                        handler.handle(getName(), line, msg);
+                        return t;
+                    }
+                    if(t.isIdentifier()) { defined.add(t); }
+                    else { return retry("Identifier expected after #define. Found: "+t); }
+                    msg = "Missing identifier after comma";
+                    t = source.nextToken();
+                } while(t == Token.COMMA && line == getLine());
+                return nextToken();
+            }
+
+
         }
-        if(notFirst) {
-            int nextLine = getLine();
-            if(line == nextLine)
-                handler.handle(getName(), line, "Directive not on its own line");
-            line = nextLine;
-            notFirst = true;
-        }
-        
-        t = source.nextToken();
-        
-        if(line != getLine())
-            return retry("Missing directive after '#'", line);
-        
-        if(t == Token.DEFINE) {
-            String msg = "Missing identifier after #define";
-            do {
-                t = source.nextToken();
-                if(line != getLine()) {
-                    handler.handle(getName(), line, msg);
-                    return t;
-                }
-                if(t.isIdentifier()) { defined.add(t); }
-                else { return retry("Identifier expected after #define. Found: "+t); }
-                msg = "Missing identifier after comma";
-                t = source.nextToken();
-            } while(t == Token.COMMA && line == getLine());
-            return nextToken();
-        }
-        
-        
+        first = false;
+        return t;
     }
 }
--- src/TokenSource.java
+++ src/TokenSource.java
@@ -5,10 +5,9 @@ public interface TokenSource {
     public Token nextToken();
     public String getName();
     public int getLine();
-    
+
     public static final class Default implements TokenSource {
         private int line;
-        private Token next;
         public final String name;
         public final StringBuilder source;
         public final ErrorHandler handler;
@@ -17,21 +16,16 @@ public interface TokenSource {
         public Default(StringBuilder source, String name, ErrorHandler handler) {
             this.source = source; this.name = name;
             this.handler = handler; line = 1;
-            next = Token.nextToken();
         }
         public Token nextToken() {
-            do {
-                if(next.isError()) { handler.handle(name, line, next.value); }
-                else if(next.isNewLine()) { line++; }
-                else if(!next.isComment()) { break; }
-                next = Token.nextToken(source);
-            } while(true);
-            return next;
-        }
-        public void clearLine() {
-            while(!next.isEOF() && !next.isNewLine()) {
-                next = Token.nextToken(source);
+            Token t;
+            while(true) {
+                t = Token.nextToken(source);
+                if(t.isError()) { handler.handle(name, line, t.value); }
+                else if(t.isNewLine()) { line++; }
+                else if(!t.isComment()) { break; }
             }
+            return t;
         }
         public static Default Get(String file, ErrorHandler handler) {
             javax.swing.JTextArea jta = new javax.swing.JTextArea();
